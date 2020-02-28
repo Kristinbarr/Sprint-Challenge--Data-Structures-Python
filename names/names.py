e0421832d75b6f4ca3d45082626a647f08db863a
Kristin Barr <kristinbarr@gmail.com>
2020-02-28 15:54:33 -0500

    names.py wip

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,11 +11,27 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# the runtime for given code is quadratic - O(n^2)
+
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# put names from 1 list in order in a BST
+# go through 2nd list and search for name in BST
+# add to list if duplicate
+
+bst = BinarySearchTree('MID')
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+
+# loop throgh names2 and search bst for each name
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
