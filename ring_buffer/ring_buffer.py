e311a39297f192d428015da8f8faef5aa8507a21
Kristin Barr <kristinbarr@gmail.com>
2020-02-28 14:15:02 -0500

    ring buffer append and get complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,44 +9,36 @@ class RingBuffer:
 
     def append(self, item):
         # if at capacity,
-        if self.capacity == self.storage.length:
-            # remove oldest from tail
-            self.storage.remove_from_tail()
-        # create new node
-        # add new node to head
-        self.storage.add_to_head(item)
-        # set head's next pointer to the tail
-        self.storage.head.prev = self.storage.tail
-        self.storage.tail.next = self.storage.head
-        # set current pointer to OLDEST node??
-        self.current = self.storage.head
-        print("\nSET finisihed, new curr head:",self.current.value)
+        if self.capacity > self.storage.length:
+            # add to tail, tail will be the right side
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            # replace the curr value
+            self.current.value = item
+            if self.current == self.storage.tail:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
+        current = self.storage.head
         # temp length variable
         size = self.storage.length
-        print('\nstarting size:', size, 'cur:', self.current.value)
-        # set current to tail
-        # self.current = self.storage.tail
         # while length is not 0,
-        # OR while current is not head?
         while size > 0:
-            # if current val is not None,
-            print('\ncur val inside while loop:', self.current.value)
-            if self.current.value is not None:
+            # don't add to list if value is None
+            if current.value is not None:
                 # add current to list
-                list_buffer_contents.append(self.current.value)
-                print('list:', list_buffer_contents)
+                list_buffer_contents.append(current.value)
             # change current to head's next
-            self.current = self.current.prev
-            print('new current:',self.current.value)
+            current = current.next
             # decrement temp length variable
             size -= 1
-            print('size:', size )
 
         return list_buffer_contents
 
