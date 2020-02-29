f8c8525e67caf71b538a86d70ba5522676f568a4
Kristin Barr <kristinbarr@gmail.com>
2020-02-28 19:29:44 -0500

    notes update for reverse.py

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -44,27 +44,35 @@ class LinkedList:
 
 
 # before:
-# (1, n:2) (2, n:3) (3, n:4) (4, n:None)
+# (5, n:4) (4, n:3) (3, n:2) (2, n:1), (1, n:None)
 
 # pre: None
-# cur: (1, n:2) -> (1, n:pre)
-# nex: (2, n:3)
-# (1, n:None) (2, n:3) (3, n:4) (4, n:None)
+# cur: (5, n:4) -> (5, n:pre)
+# nex: (4, n:3)
+# ``(5, n:None)`` (4, n:3) (3, n:2) (2, n:1) (1, n:None)
 
-# pre: (1, n:None)
-# cur: (2, n:3) -> (2, n:pre)
-# nex: (3, n:4)
-# (1, n:None) (2, n:1) (3, n:4) (4, n:None)
+# pre: (5, n:None)
+# cur: (4, n:3) -> (4, n:pre)
+# nex: (3, n:2)
+# (5, n:None) ``(4, n:5)`` (3, n:2) (2, n:1) (1, n:None)
 
-# pre: (2, n:1)
-# cur: (3, n:4) -> (3, n:pre)
-# nex: (4, n:None)
-# (1, n:None) (2, n:1) (3, n:4) (4, n:None)
+# pre: (4, n:5)
+# cur: (3, n:2) -> (3, n:pre)
+# nex: (2, n:1)
+# (5, n:None) (4, n:5) ``(3, n:4)`` (2, n:1) (1, n:None)
 
-# pre: (3, n:2)
-# cur: (4, n:None) -> (4, n:pre)
+# pre: (3, n:4)
+# cur: (2, n:1) -> (2, n:pre)
+# nex: (1, n:None)
+# (5, n:None) (4, n:5) (3, n:4) ``(2, n:3)`` (1, n:None)
+
+# pre: (2, n:3)
+# cur: (1, n:None) -> (1, n:pre)
 # nex: None
-# (1, n:None) (2, n:1) (3, n:2) (4, n:3)
+# (5, n:None) (4, n:5) (3, n:4) (2, n:3) ``(1, n:2)``
+
+# pre: (1, n:2)
+# cur: None
 
   def reverse_list(self):
 
